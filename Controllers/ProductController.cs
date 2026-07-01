cadc868276ea12b91be04d96a74c3a7c09526100
Abhishek Karande <abhishekkarande17@gmail.com>
2026-07-01 16:47:24 +0530

    Product module design Completed UI Work

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace ClotheManagementSystem.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClotheManagementSystem.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
